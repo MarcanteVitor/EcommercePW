003ad27fa928606fe38a352e4dbc7d1c1cde98f6
Vitinho_png <marcantevitor@outlook.com>
2024-03-13 19:14:26 -0300

    first commit

---
+++ server/src/main/java/br/edu/utfpr/pb/pw25s/server/model/User.java
@@ -0,0 +1,14 @@
+package br.edu.utfpr.pb.pw25s.server.model;
+
+import lombok.*;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class User {
+    private String username;
+    private String displayName;
+    private String password;
+}
