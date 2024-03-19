a114dc305a79e4b225a00515c16013c4a1d4353a
Vitinho_png <marcantevitor@outlook.com>
2024-03-18 21:41:16 -0300

    jarutais mandou comitar rs

--- server/src/main/java/br/edu/utfpr/pb/pw25s/server/model/User.java
+++ server/src/main/java/br/edu/utfpr/pb/pw25s/server/model/User.java
@@ -1,6 +1,9 @@
 package br.edu.utfpr.pb.pw25s.server.model;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 import lombok.*;
 
 @Entity
@@ -16,8 +19,17 @@ public class User {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @NotNull(message = "{br.edu.pr.utfpr.pw25s.server.user.username.NotNull}")
+    @Size(min = 4, max = 255)
     private String username;
+
+    @NotNull
+    @Size(min = 4, max = 255)
     private String displayName;
+
+    @NotNull
+    @Size(min = 6)
+    @Pattern(regexp = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d).*$")
     private String password;
 
 }
--- server/src/test/java/br/edu/utfpr/pb/pw25s/server/UserControllerTest.java
+++ server/src/test/java/br/edu/utfpr/pb/pw25s/server/UserControllerTest.java
@@ -1,5 +1,6 @@
 package br.edu.utfpr.pb.pw25s.server;
 
+import br.edu.utfpr.pb.pw25s.server.error.ApiError;
 import br.edu.utfpr.pb.pw25s.server.model.User;
 import br.edu.utfpr.pb.pw25s.server.repository.UserRepository;
 import br.edu.utfpr.pb.pw25s.server.shared.GenericResponse;
@@ -12,6 +13,10 @@ import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.ActiveProfiles;
+
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -62,6 +67,80 @@ public class UserControllerTest {
         assertThat(userRepository.count()).isEqualTo(1);
     }
 
+    @Test
+    public void postUser_whenUserIsValid_passwordIsHashedInDatabase(){
+       User user = createValidUser();
+
+       testRestTemplate.postForEntity(API_USERS, user, Object.class);
+
+        List<User> userList = userRepository.findAll();
+
+        assertThat(userList.get(0).getPassword()).isNotEqualTo(user.getPassword());
+    }
+
+    @Test
+    public void postUser_whenUserHasNullUsername_receiveBadRequest(){
+        User user = createValidUser();
+        user.setUsername(null);
+        ResponseEntity<Object> response =
+                testRestTemplate.postForEntity(API_USERS, user, Object.class);
+
+        assertThat(response.getStatusCode()).isEqualTo(HttpStatus.BAD_REQUEST);
+    }
+
+    @Test
+    public void postUser_whenUserHasNullDisplayName_receiveBadRequest(){
+        User user = createValidUser();
+        user.setDisplayName(null);
+        ResponseEntity<Object> response =
+                testRestTemplate.postForEntity(API_USERS, user, Object.class);
+
+        assertThat(response.getStatusCode()).isEqualTo(HttpStatus.BAD_REQUEST);
+    }
+
+    @Test
+    public void postUser_whenUserHasNullPassword_receiveBadRequest(){
+        User user = createValidUser();
+        user.setPassword(null);
+        ResponseEntity<Object> response =
+                testRestTemplate.postForEntity(API_USERS, user, Object.class);
+
+        assertThat(response.getStatusCode()).isEqualTo(HttpStatus.BAD_REQUEST);
+    }
+
+    @Test
+    public void postUser_whenUserHasUsernameWithLessThanRequired_receiveBadRequest(){
+        User user = createValidUser();
+        user.setUsername("abc");
+
+        ResponseEntity<Object> response =
+                testRestTemplate.postForEntity(API_USERS, user, Object.class);
+
+        assertThat(response.getStatusCode()).isEqualTo(HttpStatus.BAD_REQUEST);
+    }
+
+
+    public void postUser_whenUserHasUsernameExceededTheLengthLimit_receiveBadRequest(){
+        User user = createValidUser();
+        String usernameWith256Chars = IntStream.range(1,256).mapToObj(x -> "a").collect(Collectors.joining());
+        user.setUsername(usernameWith256Chars);
+
+        ResponseEntity<Object> response =
+                testRestTemplate.postForEntity(API_USERS, user, Object.class);
+
+        assertThat(response.getStatusCode()).isEqualTo(HttpStatus.BAD_REQUEST);
+    }
+
+    public void postUser_whenUserHasPasswordAllLowercase_receiveBadRequest(){
+        User user = createValidUser();
+        user.setPassword("password");
+
+        ResponseEntity<Object> response =
+                testRestTemplate.postForEntity(API_USERS, user, Object.class);
+
+        assertThat(response.getStatusCode()).isEqualTo(HttpStatus.BAD_REQUEST);
+    }
+
     public void postUser_whenUserIsValid_receiveSucessMessage(){
         ResponseEntity<GenericResponse> response =
                 testRestTemplate.postForEntity(
@@ -76,6 +155,24 @@ public class UserControllerTest {
                 username("test-user")
                 .displayName("test-Display")
                 .password("P4ssword").build();
+    }
+
+    @Test
+    public void postUser_whenUserIsInvalid_receiveApiError(){
+        ResponseEntity<ApiError> response =
+                testRestTemplate.postForEntity(API_USERS, new User(), ApiError.class);
+
+        assertThat(response.getBody().getUrl()).isEqualTo(API_USERS);
+
+    }
+
+    @Test
+    public void postUser_whenUserIsInvalid_receiveApiErrorWithValidationErrors(){
+        ResponseEntity<ApiError> response =
+                testRestTemplate.postForEntity(API_USERS, new User(), ApiError.class);
+
+        assertThat(response.getBody().getValidationErrors().size()).isEqualTo(3);
+
     }
 
 }
